a767257b13c945b8717600d1ee8eb6550923846f
ersati <piotrp92@gmail.com>
2021-08-05 01:56:45 +0100

    get all elements and create function play pause

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,9 +2,33 @@
 
 const player = document.querySelector(".player");
 const video = document.querySelector(".player__video");
+const progressTimeBar = document.querySelector('.progress__filled');
+const volumeBar = document.querySelector('input[name="volume"]');
+const playPauseButton = document.querySelector('.player__button');
 
+
+function videoTime(){
+    console.log(video.currentTime)
+    let time = video.currentTime;
+
+    let timerId = setInterval(() => console.log(video.currentTime), 1000);
+}
 function playVideo(){
-    video.play()
+    if(video.paused){
+        video.play()
+        
+        videoTime();
+        playPauseButton.textContent = '⏸'
+    }else {
+        video.pause()
+        playPauseButton.textContent = '▶'
+    }
+
+    console.log(video.paused)
+
 }
 
-player.addEventListener('click', playVideo)
+playPauseButton.addEventListener('click', playVideo);
+volumeBar.addEventListener('change', () => {
+    video.volume = volumeBar.value;
+})
