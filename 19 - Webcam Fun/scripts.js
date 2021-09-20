419397097dbdf5317a6caf1f3e1ab6783ef82468
ersati <piotrp92@gmail.com>
2021-09-20 01:57:25 +0100

    starts challange with fun camera

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,56 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(localMediaStream => {
+        console.log(localMediaStream)
+        video.srcObject = localMediaStream;
+        video.play()
+    }).catch(err => {
+        console.error(err)
+    })
+
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height)
+        let pixels = ctx.getImageData(0,0 , width, height)
+        // console.log(pixels)
+        pixels = redEffect(pixels)
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto () {
+    snap.currentTime = 0;
+    snap.play()
+    const data = canvas.toDataURL('image/jpeg');
+    console.log(data)
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute("download", "handsome");
+    link.textContent = 'Download image';
+    link.innerHTML = `<img src= "${data}" alt="own photo" />`
+    strip.insertBefore(link, strip.firstChild)
+
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i +=4){
+        pixels.data[i + 0]= pixels.data[i + 0] + 100;
+        pixels.data[i + 1]= pixels.data[i + 1] - 50
+        pixels.data[i + 2]= pixels.data[i + 2] * 0.5
+    }
+}
+getVideo()
+
+video.addEventListener('canplay', paintToCanvas)
