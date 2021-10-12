abfe0e99feb0c4328e0794620b449c4337264854
ersati <piotrp92@gmail.com>
2021-10-12 01:55:51 +0100

    create simple countdown

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -5,18 +5,32 @@ const display = document.querySelector('.display');
 const timeLeft = document.querySelector('.display__time-left');
 const timeEnd = document.querySelector('.display__end-time');
 
+let countdown
 function startTime(time) {
-    const timeNumber = parseInt(time)
-    const now  = new Date(); 
-    const future = now + timeNumber * 1000
+    const now = Date.now();
+    const future = now + time * 1000
     const currentTime = new Date(future)
 
-    console.log(
-        timeNumber, now,future, currentTime ,currentTime.getHours(), currentTime.getMinutes(), currentTime.getSeconds()
-    )
+    displayTime(time)
+    countdown = setInterval(() => {
+        const timeLeft = Math.round((future - Date.now()) / 1000)
+        if(timeLeft < 0 ){
+            clearInterval(countdown)
+            return
+        }
+        displayTime(timeLeft)
+    }, 1000);
+}
+
+const displayTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60
+    timeLeft.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
 
 }
-timerControls.addEventListener('click', function(e) {
-    console.log(e.target.dataset.time)
-    startTime(e.target.dataset.time)
-})
+
+const collectTime = (time) => {
+    const numberTime = parseInt(time)
+    startTime(numberTime)
+}
+timerControls.addEventListener('click', (e) => collectTime(e.target.dataset.time))
